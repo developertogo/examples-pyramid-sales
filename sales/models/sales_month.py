a260098551469c7b06430e1aaac544aef9789205
Carlos Hung <developertogo@gmail.com>
2013-12-23 10:34:16 -0800

    cleanup - remove commented / unneeded code
    Due to code refactoring

--- sales/models/sales_month.py
+++ sales/models/sales_month.py
@@ -8,19 +8,6 @@ from sqlalchemy import (
     Integer,
     Text,
     )
-'''
-from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-    )
-
-from zope.sqlalchemy import ZopeTransactionExtension
-
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-Base = declarative_base()
-'''
 
 class SalesMonth(Base):
     __tablename__ = 'sales_month'
